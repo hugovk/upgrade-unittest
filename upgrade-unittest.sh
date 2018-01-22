bc515fe8e4e89e1c05e664825b65be5609d63f80
Hugo <hugovk@users.noreply.github.com>
2018-01-22 17:44:15 +0200

    More cases

--- upgrade-unittest.sh
+++ upgrade-unittest.sh
@@ -8,6 +8,8 @@
 #
 # https://docs.python.org/2/library/unittest.html#assert-methods
 
+# Contains
+
 old='self\.assertTrue(\(.*\) not in \(.*\))'
 new='self.assertNotIn(\1, \2)'
 
@@ -27,6 +29,8 @@ old='self\.assertFalse(\(.*\) in \(.*\))'
 new='self.assertNotIn(\1, \2)'
 
 sed -i.bak "s/$old/$new/g" $1
+
+# Comparison
 
 old='self\.assertTrue(\(.*\) != \(.*\))'
 new='self.assertNotEqual(\1, \2)'
@@ -53,8 +57,44 @@ new='self.assertLessEqual(\1, \2)'
 
 sed -i.bak "s/$old/$new/g" $1
 
+old='self\.assertFalse(\(.*\) < \(.*\))'
+new='self.assertGreaterEqual(\1, \2)'
+
+sed -i.bak "s/$old/$new/g" $1
+
+old='self\.assertFalse(\(.*\) <= \(.*\))'
+new='self.assertGreater(\1, \2)'
+
+sed -i.bak "s/$old/$new/g" $1
+
+old='self\.assertFalse(\(.*\) > \(.*\))'
+new='self.assertLessEqual(\1, \2)'
+
+sed -i.bak "s/$old/$new/g" $1
+
+old='self\.assertFalse(\(.*\) >= \(.*\))'
+new='self.assertLess(\1, \2)'
+
+sed -i.bak "s/$old/$new/g" $1
+
+# Equality
+
 old='self\.assertEqual(\(.*\), None)'
 new='self.assertIsNone(\1)'
 
 sed -i.bak "s/$old/$new/g" $1
 
+old='self\.assertTrue(\(.*\) == \(.*\))'
+new='self.assertEqual(\1, \2)'
+
+sed -i.bak "s/$old/$new/g" $1
+
+old='self\.assertFalse(\(.*\) == \(.*\))'
+new='self.assertNotEqual(\1, \2)'
+
+sed -i.bak "s/$old/$new/g" $1
+
+old='self\.assertFalse(\(.*\) != \(.*\))'
+new='self.assertEqual(\1, \2)'
+
+sed -i.bak "s/$old/$new/g" $1

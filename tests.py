b88a7102dfd0249dc2fd76bced006c1b16137e23
Hugo <hugovk@users.noreply.github.com>
2018-01-22 13:23:33 +0200

    Update tests

--- tests.py
+++ tests.py
@@ -10,18 +10,18 @@ class TestStuff(unittest.TestCase):
         self.assertFalse(1 not in [1, 2])
 
     def test_comparison(self):
-        self.assertGreater(9, 1)
-        self.assertGreaterEqual(9, 1)
+        self.assertTrue(9 > 1)
+        self.assertTrue(9 >= 1)
         self.assertFalse(9 < 1)
         self.assertFalse(9 <= 1)
-        self.assertLess(1, 9)
-        self.assertLessEqual(1, 9)
+        self.assertTrue(1 < 9)
+        self.assertTrue(1 <= 9)
         self.assertFalse(1 > 9)
         self.assertFalse(1 >= 9)
 
     def test_equality(self):
         self.assertTrue(1 == 1)
-        self.assertNotEqual(1, 9)
+        self.assertTrue(1 != 9)
         self.assertFalse(1 == 9)
         self.assertFalse(1 != 1)
         self.assertEqual(None, None)

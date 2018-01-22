be05771f143aaaf5182f91aa9708bab170b90f80
Hugo <hugovk@users.noreply.github.com>
2018-01-22 14:57:17 +0200

    Update tests

--- tests.py
+++ tests.py
@@ -11,13 +11,24 @@ class TestStuff(unittest.TestCase):
 
     def test_comparison(self):
         self.assertTrue(9 > 1)
+        self.assertTrue(2 > 1)
         self.assertTrue(9 >= 1)
+        self.assertTrue(2 >= 1)
+        self.assertTrue(1 >= 1)
         self.assertFalse(9 < 1)
+        self.assertFalse(2 < 1)
         self.assertFalse(9 <= 1)
+        self.assertFalse(2 <= 1)
         self.assertTrue(1 < 9)
+        self.assertTrue(1 < 2)
         self.assertTrue(1 <= 9)
+        self.assertTrue(1 <= 2)
+        self.assertTrue(1 <= 1)
         self.assertFalse(1 > 9)
+        self.assertFalse(1 > 2)
+        self.assertFalse(1 > 1)
         self.assertFalse(1 >= 9)
+        self.assertFalse(1 >= 2)
 
     def test_equality(self):
         self.assertTrue(1 == 1)

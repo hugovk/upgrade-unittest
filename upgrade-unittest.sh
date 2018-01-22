e4096769f9bb45215c9e10ffcf2e0bde9bb6447d
Hugo <hugovk@users.noreply.github.com>
2018-01-22 17:50:00 +0200

    No need for -i's backup as next sed call changes it

--- upgrade-unittest.sh
+++ upgrade-unittest.sh
@@ -13,88 +13,89 @@
 old='self\.assertTrue(\(.*\) not in \(.*\))'
 new='self.assertNotIn(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertTrue(\(.*\) in \(.*\))'
 new='self.assertIn(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertFalse(\(.*\) not in \(.*\))'
 new='self.assertIn(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertFalse(\(.*\) in \(.*\))'
 new='self.assertNotIn(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 # Comparison
 
 old='self\.assertTrue(\(.*\) != \(.*\))'
 new='self.assertNotEqual(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertTrue(\(.*\) > \(.*\))'
 new='self.assertGreater(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertTrue(\(.*\) >= \(.*\))'
 new='self.assertGreaterEqual(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertTrue(\(.*\) < \(.*\))'
 new='self.assertLess(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertTrue(\(.*\) <= \(.*\))'
 new='self.assertLessEqual(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertFalse(\(.*\) < \(.*\))'
 new='self.assertGreaterEqual(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertFalse(\(.*\) <= \(.*\))'
 new='self.assertGreater(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertFalse(\(.*\) > \(.*\))'
 new='self.assertLessEqual(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertFalse(\(.*\) >= \(.*\))'
 new='self.assertLess(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 # Equality
 
 old='self\.assertEqual(\(.*\), None)'
 new='self.assertIsNone(\1)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertTrue(\(.*\) == \(.*\))'
 new='self.assertEqual(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertFalse(\(.*\) == \(.*\))'
 new='self.assertNotEqual(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
 
 old='self\.assertFalse(\(.*\) != \(.*\))'
 new='self.assertEqual(\1, \2)'
 
-sed -i.bak "s/$old/$new/g" $1
+sed -i "s/$old/$new/g" $1
+

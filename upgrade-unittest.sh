e62f89390e511df040abc370c8dd96824115874d
Hugo <hugovk@users.noreply.github.com>
2018-01-23 11:36:05 +0200

    Run on all input files (eg. '*.py' or '1.py 2.py')

--- upgrade-unittest.sh
+++ upgrade-unittest.sh
@@ -8,94 +8,99 @@
 #
 # https://docs.python.org/2/library/unittest.html#assert-methods
 
-# Contains
+for filename in $*
+do
+  echo "$filename"
 
-old='self\.assertTrue(\(.*\) not in \(.*\))'
-new='self.assertNotIn(\1, \2)'
+  # Contains
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertTrue(\(.*\) not in \(.*\))'
+  new='self.assertNotIn(\1, \2)'
 
-old='self\.assertTrue(\(.*\) in \(.*\))'
-new='self.assertIn(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertTrue(\(.*\) in \(.*\))'
+  new='self.assertIn(\1, \2)'
 
-old='self\.assertFalse(\(.*\) not in \(.*\))'
-new='self.assertIn(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertFalse(\(.*\) not in \(.*\))'
+  new='self.assertIn(\1, \2)'
 
-old='self\.assertFalse(\(.*\) in \(.*\))'
-new='self.assertNotIn(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertFalse(\(.*\) in \(.*\))'
+  new='self.assertNotIn(\1, \2)'
 
-# Comparison
+  sed -i "s/$old/$new/g" "$filename"
 
-old='self\.assertTrue(\(.*\) != \(.*\))'
-new='self.assertNotEqual(\1, \2)'
+  # Comparison
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertTrue(\(.*\) != \(.*\))'
+  new='self.assertNotEqual(\1, \2)'
 
-old='self\.assertTrue(\(.*\) > \(.*\))'
-new='self.assertGreater(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertTrue(\(.*\) > \(.*\))'
+  new='self.assertGreater(\1, \2)'
 
-old='self\.assertTrue(\(.*\) >= \(.*\))'
-new='self.assertGreaterEqual(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertTrue(\(.*\) >= \(.*\))'
+  new='self.assertGreaterEqual(\1, \2)'
 
-old='self\.assertTrue(\(.*\) < \(.*\))'
-new='self.assertLess(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertTrue(\(.*\) < \(.*\))'
+  new='self.assertLess(\1, \2)'
 
-old='self\.assertTrue(\(.*\) <= \(.*\))'
-new='self.assertLessEqual(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertTrue(\(.*\) <= \(.*\))'
+  new='self.assertLessEqual(\1, \2)'
 
-old='self\.assertFalse(\(.*\) < \(.*\))'
-new='self.assertGreaterEqual(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertFalse(\(.*\) < \(.*\))'
+  new='self.assertGreaterEqual(\1, \2)'
 
-old='self\.assertFalse(\(.*\) <= \(.*\))'
-new='self.assertGreater(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertFalse(\(.*\) <= \(.*\))'
+  new='self.assertGreater(\1, \2)'
 
-old='self\.assertFalse(\(.*\) > \(.*\))'
-new='self.assertLessEqual(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertFalse(\(.*\) > \(.*\))'
+  new='self.assertLessEqual(\1, \2)'
 
-old='self\.assertFalse(\(.*\) >= \(.*\))'
-new='self.assertLess(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertFalse(\(.*\) >= \(.*\))'
+  new='self.assertLess(\1, \2)'
 
-# Equality
+  sed -i "s/$old/$new/g" "$filename"
 
-old='self\.assertEqual(\(.*\), None)'
-new='self.assertIsNone(\1)'
+  # Equality
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertEqual(\(.*\), None)'
+  new='self.assertIsNone(\1)'
 
-old='self\.assertTrue(\(.*\) == \(.*\))'
-new='self.assertEqual(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertTrue(\(.*\) == \(.*\))'
+  new='self.assertEqual(\1, \2)'
 
-old='self\.assertFalse(\(.*\) == \(.*\))'
-new='self.assertNotEqual(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertFalse(\(.*\) == \(.*\))'
+  new='self.assertNotEqual(\1, \2)'
 
-old='self\.assertFalse(\(.*\) != \(.*\))'
-new='self.assertEqual(\1, \2)'
+  sed -i "s/$old/$new/g" "$filename"
 
-sed -i "s/$old/$new/g" $1
+  old='self\.assertFalse(\(.*\) != \(.*\))'
+  new='self.assertEqual(\1, \2)'
 
+  sed -i "s/$old/$new/g" "$filename"
+
+done
